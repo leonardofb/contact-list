76dcff2037ab464710d401df7014f738fdfa9cf3
leonardo fierro <planeaweb@gmail.com>
2023-05-31 15:59:10 -0500

    Actualizando lista

--- prueba.js
+++
@@ -1,6 +0,0 @@
-
-const nombre = document.getElementById('nombre').value;
-const apellido = document.getElementById('apellido').value;
-
-const mensaje = `Bienvenido/a, ${nombre} ${apellido}!`;
-document.getElementById('resultado').textContent = mensaje;
